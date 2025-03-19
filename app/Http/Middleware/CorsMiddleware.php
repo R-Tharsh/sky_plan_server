7dac3c4811a3134953d27fd8b8c337297a787f3d
robert_tharshan <it20075880@my.sliit.lk>
2025-03-19 16:53:41 +0530

    add_flight

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CorsMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $headers = [
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With',
+        ];
+
+        // Handle preflight OPTIONS request
+        if ($request->isMethod('OPTIONS')) {
+            return response()->json('CORS Preflight OK', Response::HTTP_OK, $headers);
+        }
+
+        // Proceed with normal request
+        $response = $next($request);
+        foreach ($headers as $key => $value) {
+            $response->headers->set($key, $value);
+        }
+
+        return $response;
+    }
+}
